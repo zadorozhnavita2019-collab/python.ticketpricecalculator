d532967ddada7ee3cfb7b02bd8788518a250cd88
zadorozhnavita2019-collab <zadorozhnavita2019@gmail.com>
2026-09-22 12:04:12 +0200

    Add files via upload

---
+++ ticket price.py
@@ -0,0 +1,34 @@
+num=int(input("Number of children under 13: "))
+num2=int(input("Number of people older than 13: "))
+p=input("Do u have a membership card(yes/no): ")
+
+
+if p=="yes":
+    print((num*20+num2*30)*.9)
+else:
+    print (num+num2)
+
+
+# Python Ticket Price Calculator
+
+A beginner Python program that calculates the total ticket
+price based on the number of children, adults, and membership status.
+
+## Features
+
+- Accepts the number of children under 13
+- Accepts the number of people over 13
+- Applies a membership discount
+- Calculates the total ticket price
+
+## Technologies
+
+- Python 3
+
+## What I learned
+
+- Variables
+- User input
+- Type conversion
+- Conditional statements
+- Basic arithmetic operations
